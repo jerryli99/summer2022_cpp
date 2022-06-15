c1348edd9d9d5dc92cd9c62633262f7c766bbf80
Jerry Li <84467756+jerryli99@users.noreply.github.com>
2022-06-15 15:57:33 -0400

    Update script.bash

--- practices/practice3/script.bash
+++ practices/practice3/script.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker images
-docker build . -t cpp_docker:latest
+docker build . -t practice3:latest
 clear
 echo "----------------Output---------------"
-docker run --rm -it cpp_docker:latest
+docker run --rm -it practice3:latest
 echo "-------------------------------------"
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 docker images
