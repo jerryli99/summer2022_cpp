0c8d0dfc1dac5f83e69b69b9b4840fa70f199eac
Jerry Li <84467756+jerryli99@users.noreply.github.com>
2022-06-18 00:53:43 -0400

    Delete script.bash

--- practices/practice2/script.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker images
-docker build . -t practice2:latest
-clear
-echo "----------------Output---------------"
-docker run --rm -it practice2:latest
-echo "-------------------------------------"
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
-docker images
