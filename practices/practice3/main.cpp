c817c11414692c21ea182455e3d6a123202e3ff8
Jerry Li <84467756+jerryli99@users.noreply.github.com>
2022-06-18 13:50:20 -0400

    Update main.cpp

--- practices/practice3/main.cpp
+++ practices/practice3/main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int main() {
 	StoreEmployee* employee1 = new StoreEmployee(123456);
-	StoreManager* manager1 = new StoreManager(123445, 50000.5);
-	StoreAssociate* associate1 = new StoreAssociate(443922, 35000.34);
+	StoreManager* manager1 = new StoreManager(50000, 123445);
+	StoreAssociate* associate1 = new StoreAssociate(35000.34, 444233);
 
 	cout << "employee1 id: " << employee1->get_employee_id() << "\n";
 	cout << "manager1 salary: " << manager1->get_manager_salary() << "\n";
