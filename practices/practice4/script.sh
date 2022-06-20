9eeadd01c2d96111480ed9eb0b3bdaba1820bff1
Jerry Li <84467756+jerryli99@users.noreply.github.com>
2022-06-20 18:18:55 -0400

    Update script.sh

--- practices/practice4/script.sh
+++ practices/practice4/script.sh
@@ -3,7 +3,7 @@ docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 docker images
 docker build . -t practice4:latest
 clear
-echo "----------------Output---------------"
+echo "--------------------Output---------------------"
 docker run --rm -it practice4:latest
-echo "-------------------------------------"
+echo "-----------------------------------------------"
 #docker images
