d33bc33c8ab3e291ef9cc926febb90d39d4ec63c
Jerry Li <84467756+jerryli99@users.noreply.github.com>
2022-06-20 22:54:43 -0400

    Update house.cpp

--- practices/practice5/house.cpp
+++ practices/practice5/house.cpp
@@ -8,6 +8,11 @@ House::House(double house_length, double house_width, string buyer, double land_
 House::~House() {};
 
 bool House::OverSizedHouseArea() {
+	
+	//Use Land::get_land_area() here.
+	//If I changed the name to get_area() in the Land class, and then have the 
+	//same function name get_area() in the House class, it could get conflicted
+	//and possibly get recursive function calls.
 	if ((m_house_length * m_house_width) > Land::get_land_area()) {
 		return true;
 	}
