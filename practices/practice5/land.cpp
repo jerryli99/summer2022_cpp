9a0561b6c13d11d99e0baa255b44abeb8161898d
Jerry Li <84467756+jerryli99@users.noreply.github.com>
2022-06-20 22:55:41 -0400

    Update land.cpp

--- practices/practice5/land.cpp
+++ practices/practice5/land.cpp
@@ -9,6 +9,13 @@ double Land::get_land_area() const {
 	return m_land_length * m_land_width;
 }
 
+/*
+double Land::get_area() const {
+	return m_land_length * m_land_width;
+}
+
+*/
+
 string Land::get_land_buyer() const {
 	return m_land_buyer;
 }
