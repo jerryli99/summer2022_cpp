72add3d3de25a4ebf9f4a55315f540996c58dcc8
Jerry Li <84467756+jerryli99@users.noreply.github.com>
2022-06-22 22:04:41 -0400

    Delete practices/pratice7 directory

--- practices/pratice7/root_user.cpp
+++
@@ -1,9 +0,0 @@
-#include "root_user.h"
-
-RootUser::RootUser() {};
-
-RootUser::~RootUser() {};
-
-int RootUser::get_file_permission_number() const {
-	return m_root_file_permission;
-}
--- practices/pratice7/user.cpp
+++
@@ -1,9 +0,0 @@
-#include "user.h"
-
-User::User() {};
-
-User::~User() {};
-
-int User::get_file_permission_number() const {
-	return m_user_file_permission;
-}
