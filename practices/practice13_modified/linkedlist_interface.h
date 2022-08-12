f5c0e70f08a3b3ab7c2edbf36e84959a7eed0452
Jerry Li <84467756+jerryli99@users.noreply.github.com>
2022-08-12 12:40:04 -0400

    modified some file naming and class structures
    I got rid of the UniNode and Node classes.
    Added SingleLinkedlistNode class. 
    Changed the abstract class name I_Linkedlist file and class name to Linkedlist_Interface

---
+++ practices/practice13_modified/linkedlist_interface.h
@@ -0,0 +1,26 @@
+#pragma once
+
+/*
+	This is a Linkedlist interface. Can be used when creating a
+	single, double, circular, etc linkedlists.
+*/
+namespace j_model {
+
+template<class T>
+class LinkedlistInterface {
+ public:
+	LinkedlistInterface() = default;
+	virtual ~LinkedlistInterface() = default;
+
+	//virtual void AddAtIndex(unsigned index, const T& data) = 0;
+	virtual void AddFront(const T& data) = 0;
+	virtual void AddEnd(const T& data) = 0;
+
+	//virtual void RemoveAtIndex(unsigned index) = 0;
+	virtual void RemoveFront() = 0;
+	virtual void RemoveEnd() = 0;
+
+	virtual bool contains(const T& data) = 0;
+};
+
+} //namespace j_model
---
+++ practices/practice13_modified/main.cpp
@@ -0,0 +1,82 @@
+#include "linkedlist_interface.h"
+#include "single_linkedlist_node.h"
+#include "single_linkedlist.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace j_model;
+
+//for quick testing purposes
+class Item {
+public:
+	Item(unsigned item_id) : m_item_id{ item_id } {
+		if (item_id > 999 || item_id < 0) throw "Not a valid item id.\n";
+	};
+	~Item() = default;
+
+	friend bool operator==(const Item& left, const Item& right) {
+		return (left.m_item_id == right.m_item_id);
+	}
+
+	friend std::ostream& operator<<(std::ostream& output, const Item& item) {
+		return output << "[Item id: " << item.m_item_id << "]";
+	}
+
+private:
+	unsigned m_item_id{ 0 }; // 3-digit
+};
+
+
+
+int main() {
+	//Test1
+	SingleLinkedlist<int>* test1 = new SingleLinkedlist<int>();
+	test1->AddEnd(1);
+	test1->AddFront(2);
+	test1->AddEnd(3);
+	test1->print_list();
+	delete test1;
+
+	std::cout << "\n\n";
+	//Test2
+	SingleLinkedlist<Item>* test2 = new SingleLinkedlist<Item>();
+
+	
+	for (int i = 0; i < 10; i++) {
+		test2->AddFront(*(new Item(i)));
+	}
+
+	/*
+	When i < 10000, it throws exception
+	for (int i = 0; i < 10000; i++) {
+		test2->AddFront(*(new Item(i)));
+	}
+	*/
+	test2->print_list();
+
+	std::cout << "Does the linkedlist contains Item with id: 1? " 
+		      << (test2->contains(*(new Item(1))) ? "Yes\n" : "No\n");
+
+	test2->RemoveEnd();
+	test2->RemoveFront();
+	test2->RemoveEnd();
+	test2->RemoveFront();
+
+	test2->RemoveEnd();
+	test2->RemoveFront();
+	test2->RemoveEnd();
+	test2->RemoveFront();
+
+	test2->RemoveEnd();
+	test2->RemoveFront();
+	test2->RemoveEnd();
+	test2->RemoveFront();
+
+	std::cout << "Does the linkedlist contains Item with id: 1? "
+			  << (test2->contains(*(new Item(9))) ? "Yes\n" : "No\n");
+
+	test2->print_list();
+
+	delete test2;
+}
